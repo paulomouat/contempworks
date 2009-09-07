bf3f35ed0b3b0f9d21044e5ef260261e7b144ccb
Paulo Mouat <paulo.mouat@gmail.com>
2009-09-06 22:06:06 -0400

    Added OS X script to process the xml files through the xslt. Added generated html.

---
+++ contempworks.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+xsltproc -o contempworks-a-c.html contempworks.xsl contempworks-a-c.xml
+xsltproc -o contempworks-d-f.html contempworks.xsl contempworks-d-f.xml
+xsltproc -o contempworks-g-j.html contempworks.xsl contempworks-g-j.xml
+xsltproc -o contempworks-k-m.html contempworks.xsl contempworks-k-m.xml
+xsltproc -o contempworks-n-p.html contempworks.xsl contempworks-n-p.xml
+xsltproc -o contempworks-q-s.html contempworks.xsl contempworks-q-s.xml
+xsltproc -o contempworks-t-v.html contempworks.xsl contempworks-t-v.xml
+xsltproc -o contempworks-w-z.html contempworks.xsl contempworks-w-z.xml
